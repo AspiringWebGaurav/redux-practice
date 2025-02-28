633b899094a1d0a974c723ee678f1a86d0bb5ecd
AspiringWebGaurav <gauravpatil5737@gmail.com>
2025-02-28 12:05:01 +0530

    store has been setup and slices made also wrote logic of Counter

--- redux-practice/src/redux/store/index.js
+++ redux-practice/src/redux/store/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import {configureStore} from '@reduxjs/toolkit'
+import counterReducer from '../slices/index'
 
 export default configureStore({
     reducers : {
-        //
+        counter : counterReducer
     }
 })
