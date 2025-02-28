1468a786123201f74421755b01fdc7dda205a041
AspiringWebGaurav <gauravpatil5737@gmail.com>
2025-02-28 13:13:50 +0530

    button and counter created using tailwind

--- redux-practice/src/App.jsx
+++ redux-practice/src/App.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <div className="m-80 h-10">
+        <div className="flex items-center flex-wrap justify-center gap-10 m-10">
+          <div>
+            <button
+              className="bg-transparent hover:bg-blue-500 
+          text-blue-700 font-semibold hover:text-white py-2 px-4
+           border border-blue-500 hover:border-transparent rounded"
+            >
+              Increase
+            </button>
+          </div>
+          <div>
+            <h1>Count : </h1>
+          </div>
+          <div>
+            <button
+              className="bg-transparent hover:bg-blue-500 
+          text-blue-700 font-semibold hover:text-white py-2 px-4
+           border border-blue-500 hover:border-transparent rounded"
+            >
+              Decrease
+            </button>
+          </div>
+        </div>
+        <div className="flex justify-center items-center m-5">
+          <button
+            className="bg-transparent hover:bg-blue-500 
+          text-blue-700 font-semibold hover:text-white py-2 px-4
+           border border-blue-500 hover:border-transparent rounded"
+          >
+            RESET
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default App
+export default App;
